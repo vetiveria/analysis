eb61a99abbf3e18636edf96ea19177271b6ce60f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-15 23:53:16 +0000

    Update segments/principals/linear.py

--- segments/principals/linear.py
+++ segments/principals/linear.py
@@ -2,25 +2,33 @@
 Module principals
 """
 
+import collections
+import os
+
 import numpy as np
 import pandas as pd
 import sklearn.decomposition
 
-import collections
-
+import config
 import segments.functions.margin
+import segments.functions.write
 
 
 # noinspection PyUnresolvedReferences,PyProtectedMember
 class Linear:
     """
-    Class Principals
+    Class Linear
     """
 
     def __init__(self):
         """
         The constructor
         """
+
+        configurations = config.Config()
+        self.warehousepath = configurations.warehousepath
+
+        self.write = segments.functions.write.Write()
 
         self.LPCA = collections.namedtuple(typename='LPCA', field_names=['projections', 'variance'])
 
@@ -43,8 +51,39 @@ class Linear:
         return pd.DataFrame(data={'components': components, 'explain': explain,
                                   'discrete': discrete})
 
+    def decomposition(self, data: pd.DataFrame, exclude: list):
+        """
+
+        :param data:
+        :param exclude:
+        :return:
+        """
+
+        # The independent variables
+        regressors = data.columns.drop(labels=exclude)
+
+        # Decomposition
+        pca = sklearn.decomposition.PCA(n_components=None, svd_solver='full', random_state=self.random_state)
+        model: sklearn.decomposition.PCA = pca.fit(data[regressors])
+
+        # The transform
+        transform = model.fit_transform(data[regressors])
+
+        # The variance explained by the decomposition components
+        variance: pd.DataFrame = self.variance(model=model)
+
+        return transform, variance
+
     @staticmethod
     def projections(reference: np.ndarray, transform: np.ndarray, limit: int, identifiers: list) -> pd.DataFrame:
+        """
+
+        :param reference:
+        :param transform:
+        :param limit:
+        :param identifiers:
+        :return:
+        """
 
         # The critical components
         core = transform[:, :limit].copy()
@@ -67,18 +106,7 @@ class Linear:
         :return:
         """
 
-        # The independent variables
-        regressors = data.columns.drop(labels=exclude)
-
-        # Decomposition
-        pca = sklearn.decomposition.PCA(n_components=None, svd_solver='full', random_state=self.random_state)
-        model: sklearn.decomposition.PCA = pca.fit(data[regressors])
-
-        # The transform
-        transform = model.fit_transform(data[regressors])
-
-        # The variance explained by the decomposition components
-        variance: pd.DataFrame = self.variance(model=model)
+        transform, variance = self.decomposition(data=data, exclude=exclude)
 
         # Hence, plausible number of core principal components
         index: int = self.margin.exc(values=variance.discrete.values)
@@ -87,5 +115,10 @@ class Linear:
         # Projections
         reference = data[identifiers].values.reshape(data.shape[0], len(identifiers))
         projections = self.projections(reference=reference, transform=transform, limit=limit, identifiers=identifiers)
+
+        # Write
+        path = os.path.join(self.warehousepath, 'principals', 'linear')
+        self.write.exc(blob=variance, path=path, filename='variance.csv')
+        self.write.exc(blob=projections, path=path, filename='projections.csv')
 
         return self.LPCA._make((projections, variance))
