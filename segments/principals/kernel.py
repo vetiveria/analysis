dea7d3715ea41e78dae1d0585cef9c481d2bb24d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-15 23:53:03 +0000

    Update segments/principals/kernel.py

--- segments/principals/kernel.py
+++ segments/principals/kernel.py
@@ -1,37 +1,62 @@
+"""
+Module kernel
+"""
+import collections
+import os
+
 import numpy as np
 import pandas as pd
 import sklearn.decomposition
 
-import collections
-
+import config
 import segments.functions.margin
+import segments.functions.write
 
 
 # noinspection PyUnresolvedReferences,PyProtectedMember
 class Kernel:
+    """
+    Class Kernel
+    """
 
     def __init__(self):
         """
         The constructor
         """
 
+        configurations = config.Config()
+        self.warehousepath = configurations.warehousepath
+
+        self.write = segments.functions.write.Write()
+
         self.KPCA = collections.namedtuple(typename='KPCA', field_names=['projections', 'eigenstates'])
 
         self.margin = segments.functions.margin.Margin()
         self.random_state = 5
 
-    @staticmethod
-    def eigenstates(model: sklearn.decomposition.KernelPCA):
+    def decomposition(self, data: pd.DataFrame, exclude: list):
         """
 
-        :param model:
+        :param data:
+        :param exclude:
         :return:
         """
+
+        # The independent variables
+        regressors = data.columns.drop(labels=exclude)
+
+        # Decomposition: kernel -> 'rbf', 'cosine'
+        algorithm = sklearn.decomposition.KernelPCA(kernel='rbf', eigen_solver='auto',
+                                                    random_state=self.random_state, n_jobs=-1)
+        model: sklearn.decomposition.KernelPCA = algorithm.fit(data[regressors])
+
+        # The transform
+        transform = model.fit_transform(data[regressors])
 
         eigenvalues = model.lambdas_
         components = np.arange(1, 1 + eigenvalues.shape[0])
 
-        return pd.DataFrame(data={'component': components, 'eigenvalue': eigenvalues})
+        return transform, pd.DataFrame(data={'component': components, 'eigenvalue': eigenvalues})
 
     @staticmethod
     def projections(reference: np.ndarray, transform: np.ndarray, limit: int, identifiers: list) -> pd.DataFrame:
@@ -65,19 +90,8 @@ class Kernel:
         :return:
         """
 
-        # The independent variables
-        regressors = data.columns.drop(labels=exclude)
-
-        # Decomposition: kernel -> 'rbf', 'cosine'
-        algorithm = sklearn.decomposition.KernelPCA(kernel='rbf', eigen_solver='auto',
-                                                    random_state=self.random_state, n_jobs=-1)
-        model: sklearn.decomposition.KernelPCA = algorithm.fit(data[regressors])
-
-        # The transform
-        transform = model.fit_transform(data[regressors])
-
-        # The components and their eigenvalues
-        eigenstates = self.eigenstates(model=model)
+        # Decomposing
+        transform, eigenstates = self.decomposition(data=data, exclude=exclude)
 
         # Hence, plausible number of core principal components
         index: int = self.margin.exc(values=eigenstates.eigenvalue.values)
@@ -86,5 +100,10 @@ class Kernel:
         # Projections
         reference = data[identifiers].values.reshape(data.shape[0], len(identifiers))
         projections = self.projections(reference=reference, transform=transform, limit=limit, identifiers=identifiers)
+
+        # Write
+        path = os.path.join(self.warehousepath, 'principals', 'kernel')
+        self.write.exc(blob=eigenstates, path=path, filename='eigenstates.csv')
+        self.write.exc(blob=projections, path=path, filename='projections.csv')
 
         return self.KPCA._make((projections, eigenstates))
