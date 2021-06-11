4537a0a695e6e796e97d743ad371ec593f168023
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-06-11 09:53:16 +0100

    Update segments/main.py

--- segments/main.py
+++ segments/main.py
@@ -16,30 +16,40 @@ def main():
     directories.cleanup()
     directories.create()
 
-    # Unload & Extract
+    # Unload the archive of design matrices; one per U.S. state.  Subsequently, extract.
     segments.src.read.Read().exc()
 
-    # Get the design matrix
-    design = segments.design.matrix.Matrix(descriptors=descriptors).exc()
-    logger.info('A preview of the design matrix\n{}\n'.format(design.tail().iloc[:, :7]))
+    # Get the design data sets
+    design_, original_, attributes_ = segments.design.matrix.Matrix(descriptors=descriptors).exc()
+    logger.info('A preview of the design matrix\n{}\n'.format(design_.tail().iloc[:, :7]))
 
-    # Linear dimensionality reduction
-    lpc = linear.exc(
-        data=design, exclude=descriptors.exclude, identifiers=descriptors.identifiers)
-    graphs.scatter(data=lpc.variance[:25], x='components', y='explain',
-                   labels={'x': 'the first n components', 'y': '% explained'})
+    # Data collection for building risk data sets
+    RiskData = collections.namedtuple(typename='RiskData', field_names=['design', 'original', 'attributes', 'root'])
+    data = RiskData._make((design_, original_, attributes_, risk.root))
 
-    # Nonlinear dimensionality reduction
-    kernel = segments.principals.kernel.Kernel(
-        data=design, exclude=descriptors.exclude, identifiers=descriptors.identifiers)
+    # Hence
+    options = segments.design.options.Options(data=data)
+    for group in risk.groups:
 
-    rpc = kernel.exc(kernel='rbf')
-    graphs.scatter(data=rpc.eigenstates[:15], x='component', y='eigenvalue',
-                   labels={'x': 'principal component number', 'y': 'eigenvalue'})
+        # The (a) design matrix, and (b) path, of a group
+        design, target = options.exc(group=group)
 
-    cpc = kernel.exc(kernel='cosine')
-    graphs.scatter(data=cpc.eigenstates[:15], x='component', y='eigenvalue',
-                   labels={'x': 'principal component number', 'y': 'eigenvalue'})
+        # Linear dimensionality reduction
+        lpc = linear.exc(data=design, exclude=descriptors.exclude, identifiers=descriptors.identifiers, target=target)
+        graphs.scatter(data=lpc.variance[:25], x='components', y='explain',
+                       labels={'x': 'the first n components', 'y': '% explained'})
+
+        # Nonlinear dimensionality reduction
+        kernel = segments.principals.kernel.Kernel(data=design, exclude=descriptors.exclude,
+                                                   identifiers=descriptors.identifiers)
+
+        rpc = kernel.exc(kernel='rbf', target=target)
+        graphs.scatter(data=rpc.eigenstates[:15], x='component', y='eigenvalue',
+                       labels={'x': 'principal component number', 'y': 'eigenvalue'})
+
+        cpc = kernel.exc(kernel='cosine', target=target)
+        graphs.scatter(data=cpc.eigenstates[:15], x='component', y='eigenvalue',
+                       labels={'x': 'principal component number', 'y': 'eigenvalue'})
 
 
 if __name__ == '__main__':
@@ -51,17 +61,22 @@ if __name__ == '__main__':
     Descriptors = collections.namedtuple(typename='Descriptors', field_names=['identifiers', 'exclude'])
     descriptors = Descriptors(identifiers=['COUNTYGEOID'], exclude=['COUNTYGEOID'])
 
+    import config
+
     import segments.src.directories
     import segments.src.read
 
     import segments.design.matrix
+    import segments.design.options
     import segments.principals.linear
     import segments.principals.kernel
     import segments.functions.graphs
 
+    configurations = config.Config()
+    risk = configurations.risk()
+
     directories = segments.src.directories.Directories()
     linear = segments.principals.linear.Linear()
-
     graphs = segments.functions.graphs.Graphs()
 
     main()
